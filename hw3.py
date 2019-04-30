e54fab0d9e98909a619a6e617fcb43b70cdf11aa
unknown <grantludwig97@gmail.com>
2019-04-30 15:52:18 -0700

    alg begun

--- hw3.py
+++ hw3.py
@@ -3,6 +3,7 @@
 #5/3/19
 
 import sys
+import random
 
 def readFile(fileName):
     firstLine = True
@@ -14,9 +15,42 @@ def readFile(fileName):
             bagSize = int(line)
             firstLine = False
         else:
-            iList.append(index(line))
+            iList.append(int(line))
     return (iList, bagSize)
+
+def generateChromosomes(length):
+    chromo = []
+    for _ in range(length):
+        chromo.append(bool(random.getrandbits(1)))
+    return chromo
+
+def geneticAlg(iList, size):
+    parents = []
+    fitness = []
+    for _ in range(10):
+        parents.append(generateChromosomes(len(iList)))
+    #Fitness
+    for parent in parents:
+        sum = 0
+        for i in range(len(parent)):
+            if parent[i]:
+                sum += iList[i]
+        fitness.append(sum)
+    print(fitness)
+    for i in range(len(fitness)):
+        fitness[i] = abs(size - fitness[i])
+    print(fitness)
+    #Choose best
+    parentIList = []
+    for _ in range(6):
+        index = fitness.index(min(f for f in fitness if f > 0))
+        parentIList.append(index)
+        fitness[index] = -1
+    print(parentIList)
+    #Cross
+    
 
 #Driver code
 fileName = sys.argv[1]
 itemList, sackSize = readFile(fileName)
+geneticAlg(itemList, sackSize)
